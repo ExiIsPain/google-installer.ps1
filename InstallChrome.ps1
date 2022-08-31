130921748b99c2ce4c9cc3c93c541884fb150213
ExiIsPain <112569169+ExiIsPain@users.noreply.github.com>
2022-08-31 17:19:40 +0200

    Add files via upload

---
+++ InstallChrome.ps1
@@ -0,0 +1,18 @@
+#check install
+New-Item -Path 'C:\Common Files\LOG\' -ItemType Directory
+New-Item -Path 'C:\Common Files\LOG\log.txt' -ItemType File  
+$CheckGoogle = Get-ChildItem -Path 'C:\Program Files' -Name -Filter Google 
+
+if ($CheckGoogle -contains ("Google")){
+    Add-Content -Path 'C:\Common Files\LOG\log.txt' -Value "Chrome Installed already"
+    }
+    else {
+    #download .msi
+    Invoke-WebRequest -Uri "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile 'C:\Users\Administrator\Downloads\chrome.exe'
+    #run .msi
+    <#$pathvargs = {C:\Users\Administrator\Downloads\chrome.exe /silent /install }
+    Invoke-Command -ScriptBlock $pathvargs#>
+    Start-Process -Wait -FilePath "C:\Users\Administrator\Downloads\chrome.exe" -ArgumentList "/silent /install" -PassThru
+    Add-Content -Path 'C:\Common Files\LOG\log.txt' -Value "Chrome Installed succesfully"     
+    }
+ Add-Content -Path 'C:\Common Files\LOG\log.txt' -Value "Chrome Installed"
